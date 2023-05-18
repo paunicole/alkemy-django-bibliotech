205bf4321a013148a4781b168366984218dbab55
paunicole <nicolecardozogomez@gmail.com>
2023-05-18 17:33:51 -0300

    Creación del modelo Author para el autor.

--- bibliotech/models/author.py
+++ bibliotech/models/author.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    pass
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=50, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
