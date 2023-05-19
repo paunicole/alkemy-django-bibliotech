936c714b8366d0ccf781bb0d2abb0a06a87bc6aa
JosePerezjlp <josecarlosperez.jlp@gmail.com>
2023-05-18 22:49:18 -0300

    agregando str a employee

--- bibliotech/models/employee.py
+++ bibliotech/models/employee.py
@@ -6,3 +6,6 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=100)
     file_number = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
