f6e7b8917487c8735094331921327e14aafb4d0c
santiemanuel <santiemanuel@gmail.com>
2023-05-18 22:44:57 -0300

    Agregado el método str de la clase Member

--- bibliotech/models/member.py
+++ bibliotech/models/member.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+"""Modelo para Socio de la biblioteca."""
+
 
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     birth_date = models.DateField()
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
