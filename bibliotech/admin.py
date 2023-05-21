c190ed8d1e1bf42d41fd51372c451847f6e1bb4d
santiemanuel <santiemanuel@gmail.com>
2023-05-20 22:36:42 -0300

    Registro del modelo Member en Admin.

--- bibliotech/admin.py
+++ bibliotech/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.author import Author
 from .models.employee import Employee
+from .models.member import Member
 from .models.book import Book
 
 
@@ -14,6 +15,13 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = [("first_name", "last_name"), "file_number", "is_active"]
+    search_fields = ["first_name", "last_name"]
+    list_filter = ["is_active"]
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = [("first_name", "last_name"), "birth_date", "is_active"]
     search_fields = ["first_name", "last_name"]
     list_filter = ["is_active"]
 
