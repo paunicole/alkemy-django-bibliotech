711abb4fa684586efeb2c2a6e29d7505a9ee209a
santiemanuel <santiemanuel@gmail.com>
2023-05-20 22:24:09 -0300

    Registro del modelo Author en Admin.

--- bibliotech/admin.py
+++ bibliotech/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.author import Author
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [("first_name", "last_name"), "nationality", "is_active"]
+    search_fields = ["first_name", "last_name"]
+    list_filter = ["is_active", "nationality"]
