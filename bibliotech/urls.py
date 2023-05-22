87e3ae6713c570a017543085a5d2aeaf0d77d234
santiemanuel <santiemanuel@gmail.com>
2023-05-22 20:16:53 -0300

    Creados modelos de vistas y añadidas las rutas.

--- bibliotech/urls.py
+++ bibliotech/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import employee_views
 
-urlpatterns = []
+
+urlpatterns = [
+    path("employee/new", employee_views.employee_form, name="employee_form"),
+    path("employee_list", employee_views.employee_list, name="employee_list"),
+]
---
+++ bibliotech/views/employee_views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from ..models.employee import Employee
+from ..forms.employee_form import EmployeeForm
+
+
+def employee_list(request):
+    employees = Employee.objects.all()
+    context = {"employees": employees}
+    return render(request, "employee_list.html", context)
+
+
+def employee_form(request):
+    form = EmployeeForm()
+    context = {"form": form}
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("employee_list")
+    return render(request, "employee_form.html", context)
