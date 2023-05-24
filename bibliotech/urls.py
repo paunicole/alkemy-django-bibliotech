96ae4cb32f6b75f5f047d6c8f763e2450ed97bba
JosePerezjlp <josecarlosperez.jlp@gmail.com>
2023-05-24 08:45:38 -0300

    agregando el update a empleados

--- bibliotech/urls.py
+++ bibliotech/urls.py
@@ -5,4 +5,9 @@ from .views import employee_views
 urlpatterns = [
     path("employee/new", employee_views.employee_create, name="employee-create"),
     path("employee-list", employee_views.employee_list, name="employee-list"),
+    path(
+        "employee/update/<str:employee_id>",
+        employee_views.employee_update,
+        name="employee-update",
+    ),
 ]
